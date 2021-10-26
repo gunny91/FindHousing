92067611cb009da348077cf59ae1a1985d81615a
gunny <gunny91@iastate.edu>
2021-10-26 20:34:18 +0900

    Add new springboot work as BookingHousing web

---
+++ src/main/java/com/example/findhousing/FindHousingApplication.java
@@ -0,0 +1,13 @@
+package com.example.findhousing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindHousingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindHousingApplication.class, args);
+    }
+
+}
